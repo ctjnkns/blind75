4b1ed9e72f0820493047fff8a5a683d39a1d7a80
ctjnkns <116325545+ctjnkns@users.noreply.github.com>
2023-10-08 23:26:52 -0400

    Create 21. Merge Two Sorted Lists.go

---
+++ 21. Merge Two Sorted Lists.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    dummy := new(ListNode)
+    tail := dummy
+    for list1 != nil && list2 != nil {
+        if list1.Val < list2.Val {
+            tail.Next = list1
+            list1 = list1.Next
+        } else { //list2 less
+            tail.Next = list2
+            list2 = list2.Next
+        }
+        tail = tail.Next
+    }
+    if list1 != nil {
+        tail.Next = list1
+    } else if list2 != nil {
+        tail.Next = list2
+    }
+    return dummy.Next
+}
